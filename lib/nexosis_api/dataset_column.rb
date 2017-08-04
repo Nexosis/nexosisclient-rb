c1bbed925eb264ea7a8826545b1a597fea6d2a41
Jeff Gabriel <gabriel.jeff@gmail.com>
2017-08-04 17:34:17 -0400

    add properties for aggregation and imputation

--- lib/nexosis_api/dataset_column.rb
+++ lib/nexosis_api/dataset_column.rb
@@ -3,8 +3,10 @@ module NexosisApi
     class DatasetColumn
         def initialize(column_name, value_hash)
             @name = column_name
-            @type = NexosisApi::ColumnType.const_get(value_hash["dataType"].upcase) unless value_hash["dataType"].nil?
-            @role = NexosisApi::ColumnRole.const_get(value_hash["role"].upcase) unless value_hash["role"].nil?
+            @type = NexosisApi::ColumnType.const_get(value_hash['dataType'].upcase) unless value_hash['dataType'].nil?
+            @role = NexosisApi::ColumnRole.const_get(value_hash['role'].upcase) unless value_hash['role'].nil?
+            @imputation = value_hash['imputation'] unless value_hash['imputation'].nil?
+            @aggregation = value_hash['aggregation'] unless value_hash['aggregation'].nil?
         end
         # The column header, label, or name
         # @return [String]
@@ -21,9 +23,19 @@ module NexosisApi
         # @return [NexosisApi::ColumnRole]
         attr_accessor :role
 
+        # The strategy used to imput missing values
+        # @note Either Zeroes, Mean, Median, or Mode
+        # @return [String]
+        attr_accessor :imputation
+
+        # The strategy used to aggregate data if requested prediction period is greater than observation period
+        # @note Either Sum, Mean, Median, or Mode
+        # @return [String]
+        attr_accessor :aggregation
+
         # utility method to format a column description in the way it is expected on input
         def to_hash
-            { self.name => { "dataType" => self.type.to_s, "role" => self.role.to_s }} 
+            { self.name => { 'dataType' => self.type.to_s, 'role' => self.role.to_s }} 
         end
 
         def self.to_json(column_array)
--- lib/nexosis_api/session.rb
+++ lib/nexosis_api/session.rb
@@ -1,6 +1,6 @@
 module NexosisApi
-	# Class for parsing the results of a session based request
-	class Session
+    # Class for parsing the results of a session based request
+    class Session
 		def initialize(sessionHash)
 			sessionHash.each do |k,v|
 				if(k == "links")
@@ -22,60 +22,59 @@ module NexosisApi
 					instance_variable_set("@#{k}", v) unless v.nil?
 				end
 			end
-		end
+        end
 
-		# identifier for this sesssion
-		# @return [String]
-		attr_accessor :sessionId
+        # identifier for this sesssion
+        # @return [String]
+        attr_accessor :sessionId
 
-		# What type of analysis was run during this session
-		# @return [String]
-		attr_accessor :type
+        # What type of analysis was run during this session
+        # @return [String]
+        attr_accessor :type
 
-		# Is this session requested, estimated, started, or completed
-		# @return [String]
-		attr_accessor :status
+        # Is this session requested, estimated, started, or completed
+        # @return [String]
+        attr_accessor :status
 
-		# Date and status of each status this session has entered
-		# @return [Hash]
-		attr_accessor :statusHistory
+        # Date and status of each status this session has entered
+        # @return [Hash]
+        attr_accessor :statusHistory
 
-		# reserved for future extensions
-		# @return [Hash]
-		attr_accessor :extraParameters
+        # reserved for future extensions
+        # @return [Hash]
+        attr_accessor :extraParameters
+        
+        # the dataset used in this session
+        # @return [String]
+        attr_accessor :dataSetName
 
-		# the dataset used in this session
-		# @return [String]
-		attr_accessor :dataSetName
+        # The column in the dataset for which this session ran predictions
+        # @return [String]
+        attr_accessor :targetColumn
 
-		# The column in the dataset for which this session ran predictions
-		# @return [String]
-		attr_accessor :targetColumn
+        # The start date of analysis in this session
+        # @return [DateTime]
+        attr_accessor :startDate
 
-		# The start date of analysis in this session
-		# @return [DateTime]
-		attr_accessor :startDate
+        # The end date of analysis in this session
+        # @return [DateTime]
+        attr_accessor :endDate
 
-		# The end date of analysis in this session
-		# @return [DateTime]
-		attr_accessor :endDate
+        # associated hypermedia
+        # @return [Array of NexosisApi::Link]
+        attr_accessor :links
 
-		# associated hypermedia
-		# @return [Array of NexosisApi::Link]
-		attr_accessor :links
+        # Is this session an estimate only session
+        # @return [Boolean]
+        attr_accessor :is_estimate
 
-		# Is this session an estimate only session
-		# @return [Boolean]
-		attr_accessor :is_estimate
+        # The column descriptors for the data in this session
+        #    will reflect either the metadata sent in, defaults form dataset, or inferred values
+        # @return[Array of NexosisApi::DatasetColumn]
+        attr_accessor :column_metadata
 
-		# The column descriptors for the data in this session
-		#    will reflect either the metadata sent in, defaults form dataset, or inferred values
-		# @return[Array of NexosisApi::DatasetColumn]
-		attr_accessor :column_metadata
-
-		# The requested result interval. Default is DAY if none was requested during session creation.
-		# @return [NexosisApi::TimeInterval]
-		attr_accessor :result_interval
-	end
+        # The requested result interval. Default is DAY if none was requested during session creation.
+        # @return [NexosisApi::TimeInterval]
+        attr_accessor :result_interval
+    end
 end
-
